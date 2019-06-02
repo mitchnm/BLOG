d16e214643a037d9e253ccd3f9cc4d8b758c2b85
mitchnm <mitchndungu21@gmail.com>
2019-06-02 11:47:12 +0300

    added profile page

--- app/main/views.py
+++ app/main/views.py
@@ -34,3 +34,34 @@ def new_comment(id):
         new_comment = Comments(comment=comment, user_id=current_user.id, post=post)
         new_comment.save_comment()
         return redirect(url_for('main.new_comment', id=post.id))
+    
+    title = "COMMENTS"   
+    return render_template('comments.html', comment=comment, comment_form=form, title=title, post=post)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile_page.html", user=user)
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
